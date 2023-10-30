2fbc6842593b6826adc28551fb75fd22bef14002
JonesSodaOP <148819585+JonesSodaOP@users.noreply.github.com>
2023-10-30 01:42:55 +0000

    Modified and finalized

--- lab5-1.cpp
+++ lab5-1.cpp
@@ -15,14 +15,17 @@ using namespace std;
 int main()
 {
 int num1; // num1 is not initialized
-int num2 = 5; // num2 has been initialized to 5
+int num2; // num2 is a cin value now
 cout << "Please enter an integer" << endl;
 cin >> num1;
+  cout << "Please enter another integer" << endl;
+  cin >> num2;
 cout << "num1 = " << num1 << " and num2 = " << num2 << endl;
-if (num1 = num2)
-cout << "Hey, that’s a coincidence!" << endl;
-if (num1 != num2)
-cout << "The values are not the same" << endl;
+if (num1 == num2) {
+  cout << "The values are the same." << endl;
+  cout << "Hey, that’s a coincidence!" << endl;
+} else   
+  cout << "The values are not the same" << endl;
 return 0;
 }
-// End of program
+// End of program 
